c49b445377355d8b9aafe44ee9be5dae16c29989
Leticia-Mendiola <lmendiola509@gmail.com>
2021-04-19 14:33:02 -0500

    changed database name

--- EmployeeTrackerCMS.js
+++ EmployeeTrackerCMS.js
@@ -10,7 +10,7 @@ const connection = mysql.createConnection({
   user: 'root',
   // Your password
   password: 'basicpassword509',
-  database: 'top_songsDB',
+  database: 'Employee_SchemaDB',
 });
 
 connection.connect((err) => {
