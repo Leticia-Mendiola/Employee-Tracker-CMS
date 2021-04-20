6ac37aedb08275e1016eca0abb429485267095a4
Leticia-Mendiola <lmendiola509@gmail.com>
2021-04-19 23:53:18 -0500

    filled out seeds.sql and debugged schema

--- schema.sql
+++ schema.sql
@@ -4,22 +4,25 @@ CREATE DATABASE employee_trackerDB;
 USE employee_trackerDB;
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR (30) NOT NULL,
-    role_id INT FOREIGN KEY
+    role_id INT NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE emp_role (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT FOREIGN KEY
+    department_id INT,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE department (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
+    id INT AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 SELECT * FROM employee;
--- seeds.sql
+++ seeds.sql
@@ -1,5 +1,13 @@
 USE employee_trackerDB;
 
 INSERT INTO department (name)
-VALUES ('Sales Lead'),('Salesperson'),('Lead Engineer'),('Software Engineer'),('Accountant'),('Legal Team Lead'),
-('Lawyer');
+VALUES ('Sales'),('Engineering'),('Accounting'),('Legal Team');
+
+INSERT INTO employee_role (title,salary,department_id)
+VALUES ('Sales Lead','60,000','1'),
+('Salesperson','40,000','1'),
+('Lead Engineer','100,000','2'),
+('Software Engineer','80,000','2'),
+('Accountant','60,000','3'),
+('Legal Team Lead','90,000','4'),
+('Lawyer','70,000','4');
