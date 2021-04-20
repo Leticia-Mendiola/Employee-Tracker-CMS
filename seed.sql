f6d3d07fd247d7177c5d480927180217ea39a875
Leticia-Mendiola <lmendiola509@gmail.com>
2021-04-19 23:07:07 -0500

    filled out columns to employee table

--- seed.sql
+++ seed.sql
@@ -4,7 +4,10 @@ CREATE DATABASE employee_trackerDB;
 USE employee_trackerDB;
 
 CREATE TABLE employee (
-
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR (30),
+    role_id INT FOREIGN KEY,
 );
 
 CREATE TABLE emp_role (
