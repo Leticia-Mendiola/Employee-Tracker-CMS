072d7f4d60a267451fb9ed4d53d99fdc7aaed026
Leticia-Mendiola <lmendiola509@gmail.com>
2021-04-20 00:00:40 -0500

    successfully debuged all sql code

--- seeds.sql
+++ seeds.sql
@@ -4,10 +4,10 @@ INSERT INTO department (name)
 VALUES ('Sales'),('Engineering'),('Accounting'),('Legal Team');
 
 INSERT INTO employee_role (title,salary,department_id)
-VALUES ('Sales Lead','60,000','1'),
-('Salesperson','40,000','1'),
-('Lead Engineer','100,000','2'),
-('Software Engineer','80,000','2'),
-('Accountant','60,000','3'),
-('Legal Team Lead','90,000','4'),
-('Lawyer','70,000','4');
+VALUES ('Sales Lead','60000','1'),
+('Salesperson','40000','1'),
+('Lead Engineer','100000','2'),
+('Software Engineer','80000','2'),
+('Accountant','60000','3'),
+('Legal Team Lead','90000','4'),
+('Lawyer','70000','4');
