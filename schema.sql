e1c2d6e67518224953ed6a394a42a16002d2bbd8
Leticia-Mendiola <lmendiola509@gmail.com>
2021-04-20 00:14:25 -0500

    flipped schema run

--- schema.sql
+++ schema.sql
@@ -3,15 +3,11 @@ CREATE DATABASE employee_trackerDB;
 
 USE employee_trackerDB;
 
-CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR (30) NOT NULL,
-    role_id INT NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (role_id) REFERENCES emp_role(id)
+CREATE TABLE department (
+    id INT AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
-
 CREATE TABLE emp_role (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
@@ -20,13 +16,14 @@ CREATE TABLE emp_role (
     PRIMARY KEY (id),
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
-
-CREATE TABLE department (
-    id INT AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR (30) NOT NULL,
+    role_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES emp_role(id)
 );
-
 SELECT * FROM employee;
 SELECT * FROM emp_role;
 SELECT * FROM department;
