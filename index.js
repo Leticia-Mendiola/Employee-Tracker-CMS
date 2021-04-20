69245f027853ca5c1e83acd3486aa4660e316480
Leticia-Mendiola <lmendiola509@gmail.com>
2021-04-20 00:47:33 -0500

    code cleanup

--- index.js
+++ index.js
@@ -4,11 +4,8 @@ const cTable = require('console.table');
 
 const connection = mysql.createConnection({
   host: 'localhost',
-  // Your port; if not 3306
   port: 3306,
-  // Your username
   user: 'root',
-  // Your password
   password: 'basicpassword509',
   database: 'employee_trackerdb',
 });
