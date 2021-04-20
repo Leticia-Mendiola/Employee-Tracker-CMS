91699993f521e06aa61c3e252e9ee00d75e91bf7
Leticia-Mendiola <lmendiola509@gmail.com>
2021-04-19 23:30:09 -0500

    added NOT NULL conditions

--- schema.sql
+++ schema.sql
@@ -4,22 +4,22 @@ CREATE DATABASE employee_trackerDB;
 USE employee_trackerDB;
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR (30),
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR (30) NOT NULL,
     role_id INT FOREIGN KEY
 );
 
 CREATE TABLE emp_role (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL,
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
     department_id INT FOREIGN KEY
 );
 
 CREATE TABLE department (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30)
+    name VARCHAR(30) NOT NULL
 );
 
 SELECT * FROM employee;
