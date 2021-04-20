27ec8f54af57e5655b775394e1b52456118062c0
Leticia-Mendiola <lmendiola509@gmail.com>
2021-04-19 21:13:04 -0500

    new starter code for update employee

--- index.js
+++ index.js
@@ -134,31 +134,64 @@ const addEmployee = () => {
 };
 
 const updateEmployee = () => {
-  inquirer
-    .prompt({
-      name: 'song',
-      type: 'input',
-      message: 'What song would you like to look for?',
-    })
-    .then((answer) => {
-      console.log(`You searched for "${answer.song}"`);
-      connection.query(
-        'SELECT * FROM top5000 WHERE ?',
-        { song: answer.song },
-        (err, res) => {
-          if (err) throw err;
-          if (res[0]) {
-            console.log(
-              `Position: ${res[0].position} || Song: ${res[0].song} || Artist: ${res[0].artist} || Year: ${res[0].year}`
-            );
-            runSearch();
-          } else {
-            console.error('Song not found :(\n');
-            runSearch();
+  connection.query('SELECT * FROM auctions', (err, results) => {
+    if (err) throw err;
+    // once you have the items, prompt the user for which they'd like to bid on
+    inquirer
+      .prompt([
+        {
+          name: 'choice',
+          type: 'rawlist',
+          choices() {
+            const choiceArray = [];
+            results.forEach(({ item_name }) => {
+              choiceArray.push(item_name);
+            });
+            return choiceArray;
+          },
+          message: 'What auction would you like to place a bid in?',
+        },
+        {
+          name: 'bid',
+          type: 'input',
+          message: 'How much would you like to bid?',
+        },
+      ])
+      .then((answer) => {
+        // get the information of the chosen item
+        let chosenItem;
+        results.forEach((item) => {
+          if (item.item_name === answer.choice) {
+            chosenItem = item;
           }
+        });
+
+        // determine if bid was high enough
+        if (chosenItem.highest_bid < parseInt(answer.bid)) {
+          // bid was high enough, so update db, let the user know, and start over
+          connection.query(
+            'UPDATE auctions SET ? WHERE ?',
+            [
+              {
+                highest_bid: answer.bid,
+              },
+              {
+                id: chosenItem.id,
+              },
+            ],
+            (error) => {
+              if (error) throw err;
+              console.log('Bid placed successfully!');
+              start();
+            }
+          );
+        } else {
+          // bid wasn't high enough, so apologize and start over
+          console.log('Your bid was too low. Try again...');
+          start();
         }
-      );
-    });
+      });
+  });
 };
 
 const deleteEmployee = () => {
