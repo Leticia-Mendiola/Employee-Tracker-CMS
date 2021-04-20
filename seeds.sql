a4274d9de1e223a1020abfbd8513974eb577d45a
Leticia-Mendiola <lmendiola509@gmail.com>
2021-04-19 23:23:08 -0500

    added department data to seeds.sql

---
+++ seeds.sql
@@ -0,0 +1,5 @@
+USE employee_trackerDB;
+
+INSERT INTO department (name)
+VALUES ('Sales Lead'),('Salesperson'),('Lead Engineer'),('Software Engineer'),('Accountant'),('Legal Team Lead'),
+('Lawyer');
